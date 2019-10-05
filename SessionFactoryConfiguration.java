6ca50bba702caa069f11becd53671569c8238921
Victor1s2s <pirisvictor@gmail.com>
2019-10-05 20:23:08 +0200

    Update SessionFactoryConfiguration.java

--- SessionFactoryConfiguration.java
+++ SessionFactoryConfiguration.java
@@ -8,4 +8,17 @@ public class SessionFactoryConfiguration {
     public SessionFactory getSessionFactory() {
          return entityManagerFactory.unwrap(SessionFactory.class);
     }
+    
+    public void accederBBDDsesion(){
+	    SessionFactory sessionFactory = getSessionFactory();
+        //Abrimos la sesion
+        Session session = sessionFactory.openSession();
+
+        Bill b = (Bill) session.get(Bill.class, 1);
+        System.out.println("Leida la factuta");
+        System.out.println( b.toString());
+        //Cerramos la sesion
+        session.close();
+        
+    }
 }
